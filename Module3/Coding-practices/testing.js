e2257bdcd0b0bcbc5c5e15e2ffc473b44bec227a
Adrian Sian <adriansian@gmail.com>
2021-09-08 20:51:02 +0800

    practice call function

--- Module3/Coding-practices/testing.js
+++ Module3/Coding-practices/testing.js
@@ -4,4 +4,14 @@ let android8 = {name: "oreo", api: 27}
 function displayInfo(date){
     console.log(`${this.name} was released on ${date}`)
 }
-displayInfo.apply(this, [android8, "august"]);
+displayInfo.apply(this, android8, "august");
+
+//tutorial
+var obj = {num:2}
+
+var addToThis = function (a, b, c){
+    return this.num+a+b+c;
+};
+
+console.log(addToThis.call(obj,3,4,5));
+//functionName.call (obj,functionarguments)
