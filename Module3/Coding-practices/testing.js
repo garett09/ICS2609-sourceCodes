1236bac334c2559e3b47b9828380a30ed353eafc
Adrian Sian <adriansian@gmail.com>
2021-09-12 20:10:39 +0800

    module3: apply, bind, call

--- Module3/Coding-practices/testing.js
+++ Module3/Coding-practices/testing.js
@@ -1,17 +1,38 @@
-let android9 = {name :"Pie", api: 28};
-let android8 = {name: "oreo", api: 27}
+let android9 = { name: "Pie", api: 28 };
+let android8 = { name: "oreo", api: 27 }
 
-function displayInfo(date){
+function displayInfo(date) {
     console.log(`${this.name} was released on ${date}`)
 }
-displayInfo.apply(this, android8, "august");
+let aug = ["August"]
+displayInfo.apply(android8, aug);/* this is working. call() accepts an argumentlist while apply only 
+accepts single array of arguments */
+
+try {
+    displayInfo.apply(android8, "August"); // this doesnt work.
+
+} catch (e) {
+    console.log("The command did not work.");
+}
 
 //tutorial
-var obj = {num:2}
+var obj = { num: 2 }
+var obj2 = {num:5}
 
-var addToThis = function (a, b, c){
-    return this.num+a+b+c;
+var addToThis = function (a, b, c) {
+    return this.num + a + b + c;
 };
 
-console.log(addToThis.call(obj,3,4,5));
-//functionName.call (obj,functionarguments)
+console.log(addToThis.call(obj, 3, 4, 5));
+//call
+
+var array = [3, 4, 5];
+
+console.log(addToThis.apply(obj, array));
+//apply
+
+var bound = addToThis.bind(obj);
+
+
+console.log(bound(1,2,3))
+
